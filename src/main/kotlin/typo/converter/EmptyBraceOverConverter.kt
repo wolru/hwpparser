e6ea150e9cc308dca2409c9eec587dac18fb4099
hanbitkim <hanbit2.kim@gmail.com>
2023-02-13 14:32:11 +0900

    add single brace logic

--- src/main/kotlin/typo/converter/EmptyBraceOverConverter.kt
+++ src/main/kotlin/typo/converter/EmptyBraceOverConverter.kt
@@ -4,14 +4,14 @@ import typo.TypoConverter
 
 class EmptyBraceOverConverter : TypoConverter {
     override fun convert(text: String): String {
-        val matches = Regex("([0-9]+)over([0-9]+)").findAll(text).toList()
+        val matches = Regex("([0-9]+)over([0-9]+( *\\^*(\\{[0-9]+)\\})?)").findAll(text).toList()
         if (matches.isEmpty()) {
             return text
         }
         var replacedText = text
         matches.reversed().forEach {
-            replacedText = replacedText.replaceRange(it.range.first, it.range.last + 1, "{${it.groupValues[1]}} over {${it.groupValues[2]}}")
+            replacedText = replacedText.replaceRange(it.range.first, it.range.last + 1, "{${it.groupValues[1]}}over{${it.groupValues[2]}}")
         }
-        return replacedText
+        return replacedText.replace(" ", "")
     }
 }
---
+++ src/test/kotlin/typo/converter/EmptyBraceOverConverterTest.kt
@@ -0,0 +1,23 @@
+package typo.converter
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class EmptyBraceOverConverterTest {
+
+    private val emptyBraceOverConverter = EmptyBraceOverConverter()
+
+    @Test
+    fun convertTest() {
+        val text = "1over2"
+        val convertedText = emptyBraceOverConverter.convert(text)
+        assertEquals("{1}over{2}", convertedText)
+    }
+
+    @Test
+    fun convertPowTest() {
+        val text = "1over2 ^{3}"
+        val convertedText = emptyBraceOverConverter.convert(text)
+        assertEquals("{1}over{2^{3}}", convertedText)
+    }
+}
